3c3d88837aff8e1ae3c914bca82b420b84cdcf8f
Emeli Raquel <31098448+EmeliTa@users.noreply.github.com>
2020-03-03 13:28:20 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Execute;
 
 import java.io.IOException;
@@ -12,35 +8,33 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-/**
- *
- * @author Aluno
- */
+
 public class Main extends Application {
-    private String caminhoTela = "TelaLogin.fxml";
-    private String tituloTela = "Login";
-    
-    public static void main(String[] args) {
+        private String caminhoTela = "TelaLogin.fxml";
+        private String tituloTela = "Login";
+
+public static void main(String[] args) {
         launch(args);
     }
-               @Override
-    public void start(Stage stage) throws IOException {
+@Override
+public void start(Stage stage) throws IOException {
         Parent root = FXMLLoader.load(getClass().getResource("/View/"+caminhoTela));
         Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.show();
         stage.setTitle(tituloTela);
     }
-    public void abreTela (String nomeTela, String Titulo) {
+public void abreTela (String nomeTela, String Titulo) {
         caminhoTela = nomeTela;
         tituloTela = Titulo;
         Stage stage = new Stage();
-        try {
-            start(stage);
-            stage.setTitle(tituloTela);
-        } catch (Exception e) {
-            System.out.println("Erro ao abrir tela!" + e.getMessage());
-        }
+ try {
+                            start(stage);
+                            stage.setTitle(tituloTela);
+     }
+ catch (Exception e) {
+                            System.out.println("Erro ao abrir tela!" + e.getMessage());
+                     }
     }
     
 }
